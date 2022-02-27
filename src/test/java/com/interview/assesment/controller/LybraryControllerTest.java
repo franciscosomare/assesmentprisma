6a6cf300146077d745289af8247686b1b59a462e
Francisco Somare <fsomare@paypal.com>
2022-02-27 23:13:10 +0100

    FS - add siple uTest

--- src/test/java/com/interview/assesment/controller/LybraryControllerTest.java
+++ src/test/java/com/interview/assesment/controller/LybraryControllerTest.java
@@ -1,4 +1,56 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.interview.assesment.controller;
+
+import com.interview.assesment.service.LibraryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = LybraryController.class)
 class LybraryControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LibraryService libraryService;
+
+    @Test
+    public void shouldCallgetUsersWhoBorrowedAtLeastABookEndPointCorrectly() throws Exception {
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/v1/library/borrowed/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldCallgetUsersWhoNotBorrowedAbookCorrectly() throws Exception {
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/v1/library/not-borrowed/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldCallgetUsersWhoNotBorrowedAbookCorrectly2() throws Exception {
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/v1/library/not-borrowed/users/date?date=05/14/2008")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
 }
--- src/test/java/com/interview/assesment/service/impl/LibraryServiceImplTest.java
+++ src/test/java/com/interview/assesment/service/impl/LibraryServiceImplTest.java
@@ -1,23 +1,48 @@
 package com.interview.assesment.service.impl;
 
+import com.interview.assesment.model.UserLibrary;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class LibraryServiceImplTest {
 
     private final LibraryServiceImpl libraryService = new LibraryServiceImpl();
 
     @Test
-    public void test1() throws IOException {
-        // given
-
-
+    public void shouldGetAllUsersWhoBorrewedAtLeastABookCorrectly() throws IOException {
         // when
-        libraryService.getUsersWhoBorrowedAtLeastABook();
+        List<UserLibrary> userLibraries = libraryService.getUsersWhoBorrowedAtLeastABook();
 
         // then
+        assertEquals(11, userLibraries.size());
+    }
 
+    @Test
+    public void shouldGetAllUsersWhoNotBorrewedAtLeastABookCorrectly() throws IOException {
+        // when
+        List<UserLibrary> userLibraries = libraryService.getUsersWhoNotBorrowedAbook();
+
+        // then
+        assertEquals(0, userLibraries.size());
+    }
+
+    @Test
+    public void shouldGetAllUsersWhoBorrewedAtLeastABookCorrectlyOnAGivenDate() throws IOException {
+        // given
+        LocalDate date = LocalDate.of(2008, 05, 14);
+        // when
+        List<UserLibrary> userLibraries = libraryService.getUserWhoBorrowedABookInADate(date);
+
+        // then
+        assertEquals(1, userLibraries.size());
+        assertEquals("Chish", userLibraries.get(0).getName());
+        assertEquals("Elijah", userLibraries.get(0).getFirstName());
     }
 
 }
